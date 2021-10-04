36f5d5baf04766055d9c9641a0b9e23483c74dbd
Benjamin Jones <benjamin.jones@clifton-asset.co.uk>
2021-10-04 14:13:01 +0100

    finished all parts of project

---
+++ app.js
@@ -0,0 +1,43 @@
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+//the replace removes non-alphanumeric characters
+let textArr = argv.text.toLowerCase().replace(/[^\w\s]/gi, '').split("");
+let flag = false; 
+
+//changing textArr to only have letters from beginning and end of words
+for (let i = 0; i < textArr.length; i++) {
+    if (textArr[i] !== " ") {
+        flag = true;
+        while (flag) {
+            if (textArr[i+1] !== " " && textArr[i+2] !== " " && i+1 !== textArr.length-1) {
+                textArr.splice(i+1, 1);
+            } else {
+                flag = false;
+            }
+        }
+        i++;
+    } else {
+        textArr.splice(i, 1);
+        i--;
+    }
+}
+
+let messageArr = argv.message.toLowerCase().replace(/[^\w\s]/gi, '').split(""); // the /\s+/ will remove all whitespace
+while (messageArr.includes(" ")) {
+    messageArr.splice(messageArr.indexOf(" "), 1);
+}
+
+function acrosticMessageCheck() {
+    for (let letter of messageArr) {
+        if (textArr.includes(letter)) {
+            textArr.splice(textArr.indexOf(letter), 1);
+        } else {
+            return false;
+        }
+    } return true;
+}
+
+
+console.log(acrosticMessageCheck());
